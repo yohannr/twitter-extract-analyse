cfb2042f1f096d612317b4f74484e72eb8361597
yohannr <kozher20@hotmail.com>
2014-08-26 15:46:27 +0200

    Add first analyze
    Add first analyze : 'Among friends, which are those who did not tweet
    for a while'
    + new parameter (TWITTER_NAME) in config file

--- README.md
+++ README.md
@@ -17,4 +17,4 @@ Twitter processing library : extract & analyse data from an account : friends, f
 - because of twitter rate limit, once 150 requests the process is paused.
 For this reason, this script is not recommended for account with high number of friends/followers and will be likely execute throught PHP CLI
 
-- Define in a config file all the twitter keys : CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET
+- Define in a config file all the twitter keys (CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET) and the account name (TWEETER_NAME)
--- run.php
+++ run.php
@@ -6,10 +6,23 @@ require_once('config.php');
 $twitter = new Twitter(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET);
 
 $arr_friends = array();
-$tweeter_name = 'twamingetc';
+$twitter_name = TWITTER_NAME;
 
-$arr_friends = $twitter->friendsList($tweeter_name);
+$arr_friends = $twitter->friendsList($twitter_name);
+
+
+// Among friends, which are those who did not tweet for a while (ex : 1 month ago), cf. 'last_tweet'
+$arr_removeFriends = array();
+$delay = 30;
+foreach ($arr_friends as $friend) {
+	$duration = time() - $friend['last_tweet'];
+	$duration = round($duration/3600/24);
+
+	if ($duration > 30) {
+		$arr_remove[] = $friend['screen_name'];
+	}
+}
 
 echo "\n";
-print_r($arr_friends[0]);
+print_r($arr_remove);
 echo "\n";
